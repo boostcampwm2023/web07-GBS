fd21ddff27251c85592e813c0508a50cef0d3bbe
ChoiSangwon <fdsa200@naver.com>
2023-11-09 17:03:47 +0900

    remove : 필요없는 초기 파일 제거

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>
+);
