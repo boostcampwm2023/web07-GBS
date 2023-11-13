71e9b3fbb24f765c7580d73b3f36ef3017ee2349
kkg5 <kkg05102@gmail.com>
2023-11-13 22:31:46 +0900

    feat : 스트리밍/인코딩 서버 세팅 완료
    - node-media-server 라이브러리 적용
    - 스트리밍 송신, 인코딩 수신 테스트
    - 스트리밍/인코딩 서버 CI/CD 추가

--- server/rtmp-server/app.js
+++ server/rtmp-server/app.js
@@ -1,24 +1,25 @@
-const NodeMediaServer = require('node-media-server');
+require("dotenv").config();
+const NodeMediaServer = require("node-media-server");
 
 const config = {
-    rtmp: {
-        port: 1935,
-        chunk_size: 60000,
-        gop_cache: true,
-        ping: 30,
-        ping_timeout: 60
-    },
-    relay: {
-        ffmpeg: '/usr/local/bin/ffmpeg',
-        tasks: [
-            {
-                app: 'live',
-                mode: 'push',
-                edge: 'rtmp:127.0.0.1:19350', // TODO:
-            }
-        ]
-    }
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60,
+  },
+  relay: {
+    ffmpeg: "/usr/local/bin/ffmpeg",
+    tasks: [
+      {
+        app: "live",
+        mode: "push",
+        edge: "rtmp://" + process.env.ENCODING_SERVER_HOST,
+      },
+    ],
+  },
 };
 
-const nms = new NodeMediaServer(config)
+const nms = new NodeMediaServer(config);
 nms.run();
