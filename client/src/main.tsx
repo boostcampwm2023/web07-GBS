d1ff486dd26dc89d50339217538443d7802bc167
ChoiSangwon <fdsa200@naver.com>
2023-11-14 16:49:37 +0900

    [feat] 프론트엔드 라우터 세팅 (#21)
    * chore : 라우팅을 위한 react-router-dom 설치
    
    * feat : 라우터 사용해서 페이지 분리하기

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-   <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-   </React.StrictMode>
-);
+    </BrowserRouter>
+  </React.StrictMode>,
+)
---
+++ client/src/pages/ExPage/ExPage.styles.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #b1b1b1;
+`
