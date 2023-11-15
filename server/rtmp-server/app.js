c7557bd68f4e4f0532953c6d6e683bf4e8e6a3eb
Joung MyeongHee <jmhee3411@gmail.com>
2023-11-15 16:22:26 +0900

    fix: H264 화면 송출 에러 fix

--- server/rtmp-server/app.js
+++ server/rtmp-server/app.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 const NodeMediaServer = require("node-media-server");
+let exec = require('child_process').exec
+
+exec('node system_func.js', (err,out,stderr) => {
+  console.log(out)
+});
 
 const config = {
   rtmp: {
@@ -9,16 +14,28 @@ const config = {
     ping: 30,
     ping_timeout: 60,
   },
-  relay: {
-    ffmpeg: "/usr/local/bin/ffmpeg",
-    tasks: [
-      {
-        app: "live",
-        mode: "push",
-        edge: "rtmp://" + process.env.ENCODING_SERVER_HOST,
-      },
-    ],
+  http: {
+    port: 80,
+    mediaroot: __dirname + "/media",
+    webroot: __dirname + "/www",
+    allow_origin: "*",
+    api: true,
   },
+    trans: {
+      ffmpeg: "/usr/local/bin/ffmpeg",
+      tasks: [
+        {
+          app: "live",
+          vc: "copy",
+          vcParam: [],
+          ac: "aac",
+          acParam: ["-ab", "64k", "-ac", "1", "-ar", "44100"],
+          hls: true,
+          hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+        },
+      ],
+    },
+
 };
 
 const nms = new NodeMediaServer(config);
