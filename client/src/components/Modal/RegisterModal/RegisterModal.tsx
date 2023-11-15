b27c90c7e69a0b0814b7178e1049d3ed553dcfbb
ChoiSangwon <fdsa200@naver.com>
2023-11-15 14:36:42 +0900

    feat : 회원가입 모달 제작

---
+++ client/src/components/Modal/RegisterModal/RegisterModal.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+import * as styles from './RegisterModal.styles'
+
+interface RegisterModalProps {
+  /**
+   * 취소 버튼 눌렀을 때 실행되는 함수
+   */
+  onCancle: () => void
+  /**
+   * 확인 버튼 눌렀을 때 실행되는 함수
+   */
+  onConfirm: () => void
+}
+
+const RegisterModal = ({ onCancle, onConfirm }: RegisterModalProps) => {
+  const [id, setId] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [confirmPassword, setConfirmPassword] = useState<string>('')
+
+  return (
+    <styles.Backdrop onClick={onCancle}>
+      <styles.Modal>
+        <styles.ModalView
+          onClick={(e) => {
+            e.stopPropagation()
+          }}
+        >
+          <styles.Container>
+            <styles.HeaderText>회원가입</styles.HeaderText>
+            <styles.InputBox>
+              <styles.BodyText>ID</styles.BodyText>
+              <styles.Input
+                value={id}
+                onChange={(e) => {
+                  setId(e.target.value)
+                }}
+              />
+            </styles.InputBox>
+            <styles.InputBox>
+              <styles.BodyText>비밀번호</styles.BodyText>
+              <styles.Input
+                value={password}
+                type="password"
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+              />
+            </styles.InputBox>
+            <styles.InputBox>
+              <styles.BodyText>비밀번호 확인</styles.BodyText>
+              <styles.Input
+                value={confirmPassword}
+                type="password"
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value)
+                }}
+              />
+            </styles.InputBox>
+          </styles.Container>
+          <styles.ButtonContainer>
+            <styles.Button onClick={onConfirm} style={{ borderBottomLeftRadius: '10px' }}>
+              확인
+            </styles.Button>
+            <styles.Button onClick={onCancle} style={{ borderBottomRightRadius: '10px' }}>
+              취소
+            </styles.Button>
+          </styles.ButtonContainer>
+        </styles.ModalView>
+      </styles.Modal>
+    </styles.Backdrop>
+  )
+}
+export default RegisterModal
