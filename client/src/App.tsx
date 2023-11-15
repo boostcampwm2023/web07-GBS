64bfc80c2d11600f5e612f031c4626af54e98885
top-chaser <2000dnjswns@gmail.com>
2023-11-15 11:52:35 +0900

    feat : 전역 스타일링 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage/HomePage'
-import ExPage from './pages/ExPage/ExPage'
+
+import GlobalStyle from '/src/styles/GlobalStyles.styles'
+import HomePage from '/src/pages/HomePage/HomePage'
+import ExPage from '/src/pages/ExPage/ExPage'
+import BroadcastPage from '/src/pages/BroadcastPage/BroadcastPage'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/exPage" element={<ExPage />} />
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/exPage" element={<ExPage />} />
+        <Route path="/broadcastPage" element={<BroadcastPage />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ client/src/pages/BroadcastPage/BroadcastPage.tsx
@@ -0,0 +1,11 @@
+import * as styles from './BroadcastPage.styles'
+
+const BroadcastPage = () => {
+  return (
+    <styles.Container>
+      <h1>exPage</h1>
+    </styles.Container>
+  )
+}
+
+export default BroadcastPage
