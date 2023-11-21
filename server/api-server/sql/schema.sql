2d74de0d4f118bfb0b520963a98918fab7d8b9e5
kkg5 <kkg05102@gmail.com>
2023-11-21 15:06:08 +0900

    fix: schema.sql 수정

--- server/api-server/sql/schema.sql
+++ server/api-server/sql/schema.sql
@@ -12,36 +12,39 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- test 데이터베이스 구조 내보내기
+
+-- gbs 데이터베이스 구조 내보내기
 CREATE DATABASE IF NOT EXISTS `gbs` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `gbs`;
 
+-- 테이블 gbs.stream 구조 내보내기
 CREATE TABLE IF NOT EXISTS `stream` (
-  `stream_key` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stream_key` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `title` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `desc` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `deleted_at` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`stream_key`),
-  UNIQUE KEY `IDX_c4b17b4aa04c321f096a09cbe3` (`user_id`)
+  PRIMARY KEY (`stream_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- 테이블 test.user 구조 내보내기
+-- 테이블 gbs.user 구조 내보내기
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nickname` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stream_key` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_id` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `oauth_id` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `oauth_type` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nickname` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `deleted_at` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `IDX_e2364281027b926b879fa2fa1e` (`nickname`),
+  UNIQUE KEY `IDX_758b8ce7c18b9d347461b30228` (`user_id`),
   UNIQUE KEY `IDX_95d512d160789656ed4f21af99` (`oauth_id`),
-  UNIQUE KEY `IDX_758b8ce7c18b9d347461b30228` (`user_id`)
+  UNIQUE KEY `IDX_e2364281027b926b879fa2fa1e` (`nickname`),
+  UNIQUE KEY `REL_7aac5bf14432c1aad0bb8cb933` (`stream_key`),
+  CONSTRAINT `FK_7aac5bf14432c1aad0bb8cb933b` FOREIGN KEY (`stream_key`) REFERENCES `stream` (`stream_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
