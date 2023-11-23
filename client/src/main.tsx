79a469ba1fb55ad211055967ea7c7d5bf2c11bfc
top-chaser <2000dnjswns@gmail.com>
2023-11-23 17:08:20 +0900

    refactor : React.StrictMode 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,12 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.tsx'
 import { RecoilRoot } from 'recoil'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>,
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>,
 )
