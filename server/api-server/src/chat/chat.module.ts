773f28bd9bb68af63d7f80f9eab6808f011a3964
kkg5 <kkg05102@gmail.com>
2023-11-28 17:35:46 +0900

    feat: 방송 조회 viewer 연결

--- server/api-server/src/chat/chat.module.ts
+++ server/api-server/src/chat/chat.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [ChatGateway],
+  providers: [ChatGateway, UsersModule],
+  exports: [ChatGateway]
 })
 export class ChatModule {}
