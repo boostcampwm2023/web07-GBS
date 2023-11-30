383a61922e41cf1818e983e13dba4f8b05aff0d5
ChoiSangwon <fdsa200@naver.com>
2023-11-30 17:22:51 +0900

    feat : 커스텀훅 사용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,12 @@ import GlobalStyle from '@/styles/GlobalStyles.styles'
 import MainPage from '@/pages/MainPage/MainPage'
 import BroadcastPage from '@/pages/BroadcastPage/BroadcastPage'
 import { ThemeProvider } from 'styled-components'
-import { useRecoilValue } from 'recoil'
-import { themeState, ThemeFlag } from '@/states/theme'
 import { lightTheme, darkTheme } from '@/styles/theme'
+import useStoredTheme from '@/hooks/useStoredTheme'
+import { ThemeFlag } from './states/theme'
 
 function App() {
-  const currentTheme = useRecoilValue(themeState)
+  const currentTheme = useStoredTheme()
   const theme = currentTheme === ThemeFlag.light ? lightTheme : darkTheme
 
   return (
