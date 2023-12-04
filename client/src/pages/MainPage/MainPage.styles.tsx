2a1b2f6708b76fa62856a065572a90f07094ca02
ChoiSangwon <fdsa200@naver.com>
2023-12-04 14:09:45 +0900

    fix : 방송 리스트 길이가 0일때 EmptyList 띄우기 및 스타일 변경

--- client/src/pages/MainPage/MainPage.styles.tsx
+++ client/src/pages/MainPage/MainPage.styles.tsx
@@ -2,34 +2,41 @@ import { ThemeFlag } from '@/states/theme'
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100vw;
   height: 100vh;
 `
 
-export const Logo = styled.div`
-  position: absolute;
-  top: 1.5625rem;
-  left: 41rem;
+export const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
 `
+
+export const Logo = styled.div``
 
 export const Access = styled.div`
   position: absolute;
-  top: 7.0625rem;
-  left: 63.25rem;
+  left: 50%;
+  transform: translateX(80%);
 `
 
 interface ListProps {
   currentTheme: ThemeFlag
+  length: number
 }
 
 export const List = styled.div<ListProps>`
   display: flex;
   flex-direction: column;
   border: ${(props) => {
+    if (props.length === 0) return
     if (props.currentTheme === ThemeFlag.light) return '0.0625rem solid #000000'
     else return '0.0625rem solid #ffffff'
   }};
-  position: absolute;
-  top: 15.625rem;
-  left: 18.75rem;
+  justify-content: center;
+  align-items: center;
+  margin: 10rem auto;
 `
