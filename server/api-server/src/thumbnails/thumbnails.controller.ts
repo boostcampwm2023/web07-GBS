5726199db01a35ce63e29501d34175c5d4ab59f9
Joung MyeongHee <jmhee3411@gmail.com>
2023-12-07 03:47:14 +0900

    feat: thumbnail return API 생성

--- server/api-server/src/thumbnails/thumbnails.controller.ts
+++ server/api-server/src/thumbnails/thumbnails.controller.ts
@@ -6,18 +6,7 @@ export class ThumbnailsController {
   constructor(private readonly thumbnailsService: ThumbnailsService) {}
 
   @Get(':userId')
-  async findAll(@Param('userId') userId: string) {
-    const object_name = `thumbnails/${userId}.jpg`;
-    await this.thumbnailsService.extractTsUrls(userId);
-    const filePath = `${process.cwd()}/src/thumbnails/thumbnail.jpg`;
-
-    if (fs.existsSync(filePath)) {
-      await this.thumbnailsService.uploadS3(filePath, object_name);
-      await this.thumbnailsService.removeFile(filePath);
-      return `${process.env.AWS_S3_URL}${process.env.AWS_S3_BUCKET_NAME}/thumbnails/${userId}.jpg`
-    } else {
-      return 'thumbnail not found'
-    }
-    
+  async getThumbnailUrl(@Param('userId') userId: string) {
+    return await this.thumbnailsService.getThumbnailUrl(userId);    
   }
 }
--- server/api-server/src/thumbnails/thumbnails.service.ts
+++ server/api-server/src/thumbnails/thumbnails.service.ts
@@ -8,78 +8,15 @@ import * as fs from 'fs';
 
 @Injectable()
 export class ThumbnailsService {
-  async extractTsUrls(userId: string) {
-    try {
+  
+  async getThumbnailUrl(userId: string) {
 
-      const prefix = `${process.env.AWS_S3_URL}${process.env.AWS_S3_BUCKET_NAME}`
-      const prefixURL = `${prefix}/hls`;
-
-      const m3u8Url = `${prefixURL}/${userId}.m3u8`;
-
-      const response = await fetch(m3u8Url);
-      const body = await response.text();     
-      const lines = body.split('\n');
-
-      const newM3U8 = lines[3];
-      const newResponse = await fetch(
-        `${prefixURL}/${newM3U8}`,
-      );
-      const newBody = await newResponse.text();
-      const newLines = newBody.split('\n');
-
-      const temp = newM3U8.split('/');
-      const arg = `${prefixURL}/${temp[0]}/${newLines[5]}`;
-
-      await this.executeShellScript(
-        `${process.cwd()}/src/thumbnails/makeThumbnail.sh`,
-        `${arg}`
-      );
-
-    } catch (error) {
-      console.error('Error fetching M3U8 file:', error);
-    }
-  }
-
-  executeShellScript(scriptPath: string, args: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      exec(`sh ${scriptPath} ${args}`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Execution error: ${error.message}`);
-          reject(error);
-          return;
-        }
-        if (stderr) {
-          console.error(`Error: ${stderr}`);
-          reject(stderr);
-          return;
-        }
-        console.log(`Output: ${stdout}`);
-        resolve('thumbnail saved'); // Resolve the promise with the output
-      });
-    });
-  }
-
-  removeFile(filePath: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      unlink(filePath, (err) => {
-        if (err) {
-          console.error(`Error deleting file ${filePath}:`, err);
-          reject(err);
-        } else {
-          console.log(`File ${filePath} deleted successfully`);
-          resolve();
-        }
-      });
-    });
-  }
-
-  async uploadS3(filePath: string, object_name: string) {
-
+    const object_name = `thumb/${userId}_240p264kbs.png`;
+    console.log(object_name);
     const endpoint = new AWS.Endpoint(process.env.AWS_S3_URL);
     const region = process.env.AWS_S3_REGION;
-    const access_key = process.env.AWS_ACCESS_KEY_ID; 
-    const secret_key = process.env.AWS_SECRET_ACCESS_KEY; 
-
+    const access_key = process.env.AWS_ACCESS_KEY_ID;
+    const secret_key = process.env.AWS_SECRET_ACCESS_KEY;
     const S3 = new AWS.S3({
       endpoint: endpoint,
       region: region,
@@ -89,13 +26,14 @@ export class ThumbnailsService {
       }
     });
     
-    await S3.putObject({
-        Bucket: process.env.AWS_S3_BUCKET_NAME,
-        Key: object_name,
-        Body: fs.createReadStream(filePath),
-        ACL: 'public-read',
-    }).promise();
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: object_name,
+    };
+
+    const objectInfo = await S3.getObject(params).promise();
+        
+    return {ContentLength: objectInfo.ContentLength, thumbnailUrl: object_name}
 
   }
-
 }
