7c105f20a2fbd6046293cf5cf82e0c61f265e559
Kim KyeongGeun <kkg05102@gmail.com>
2023-12-07 13:52:44 +0900

    [feat] AI 채팅 검열 구현 (#159)
    * feat: AI 채팅 검열 구현
    
    * fix: axios 예외 처리

--- server/api-server/src/chat/chat.gateway.ts
+++ server/api-server/src/chat/chat.gateway.ts
@@ -9,11 +9,12 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { JoinPayload } from './dto/join-payload.dto';
-import { ChatPayload } from './dto/chat-payload';
+import { ChatPayload } from './dto/chat-payload.dto';
 import { Logger } from '@nestjs/common';
 import { User } from 'src/users/entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AIChatFilter } from './util/ai-chat-filter';
 
 @WebSocketGateway({
   cors: {
@@ -22,7 +23,10 @@ import { Repository } from 'typeorm';
   },
 })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private userRepo: Repository<User>,
+    private aiChatFilter: AIChatFilter,
+  ) {}
 
   @WebSocketServer() server: Server = new Server({ cookie: true });
   private readonly logger = new Logger(ChatGateway.name);
@@ -64,10 +68,12 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   ): Promise<ChatPayload> {
     this.logger.debug('Chat payload: ', payload);
 
+    const result = await this.aiChatFilter.filter(payload.message);
+
     this.server.to(client.data.room).emit('chat', {
       id: client.data.userId,
       nickname: client.data.nickname,
-      message: payload.message,
+      ...result,
     });
     return payload;
   }
--- server/api-server/src/chat/chat.module.ts
+++ server/api-server/src/chat/chat.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { UsersModule } from 'src/users/users.module';
+import { AIChatFilter } from './util/ai-chat-filter';
 
 @Module({
   imports: [UsersModule],
-  providers: [ChatGateway],
+  providers: [ChatGateway, AIChatFilter],
   exports: [ChatGateway],
 })
 export class ChatModule {}
