9c6ef05ad7587f378bbc9ca5a2a6dc43a0b60217
ChoiSangwon <fdsa200@naver.com>
2023-12-07 14:03:11 +0900

    feat : id props로 추가 및 css스타일 변경

--- client/src/components/HlsPlayer/HlsPlayer.tsx
+++ client/src/components/HlsPlayer/HlsPlayer.tsx
@@ -1,32 +1,55 @@
-import { useEffect, useRef } from 'react';
-import Hls from 'hls.js';
+import { useEffect, useRef } from 'react'
+import Hls from 'hls.js'
+import styled from 'styled-components'
 
-const HlsPlayer = () => {
-  const videoSrc = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
-  const videoRef = useRef<HTMLVideoElement>(null);
+interface HlsPlayerProps {
+  id?: string
+}
+
+const HlsPlayer = ({ id }: HlsPlayerProps) => {
+  const videoSrc = `${import.meta.env.VITE_HLS_URL}/${id}`
+  const videoRef = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
-    const video = videoRef.current;
-    let hls:Hls;
-
+    const video = videoRef.current
+    let hls: Hls
+    console.log(videoSrc)
     if (video) {
       if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src = videoSrc;
+        video.src = videoSrc
       } else if (Hls.isSupported()) {
-        hls = new Hls();
-        hls.loadSource(videoSrc);
-        hls.attachMedia(video);
+        hls = new Hls()
+        hls.loadSource(videoSrc)
+        hls.attachMedia(video)
       }
 
       return () => {
         if (hls) {
-          hls.destroy();
+          hls.destroy()
         }
-      };
+      }
     }
-  }, []);
+  }, [])
 
-  return <video ref={videoRef} controls />;
-};
+  return (
+    <Container>
+      <Video ref={videoRef} controls />
+    </Container>
+  )
+}
 
-export default HlsPlayer;
+export default HlsPlayer
+
+const Container = styled.div`
+  position: relative;
+  padding-bottom: 56.25%;
+  padding-top: 9rem;
+`
+
+const Video = styled.video`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
