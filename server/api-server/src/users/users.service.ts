8adcb3cd961d3be3ffaf36a48f6e7c9ce1b1395e
Joung MyeongHee <jmhee3411@gmail.com>
2023-12-07 21:53:05 +0900

    fix: console log 삭제

--- server/api-server/src/users/users.service.ts
+++ server/api-server/src/users/users.service.ts
@@ -51,7 +51,7 @@ export class UsersService {
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
-    console.log(`params: ${id}, body: ${JSON.stringify(updateUserDto)}`);
+
     const user = await this.findOne(id);
     if (!user) {
       throw new HttpException('User not found', HttpStatus.NOT_FOUND);
