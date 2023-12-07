bfb936712e9606c0de0710e0cc543176011ef110
Joung MyeongHee <jmhee3411@gmail.com>
2023-12-07 11:19:26 +0900

    fix: camelCase로 변경

--- server/api-server/src/thumbnails/thumbnails.service.ts
+++ server/api-server/src/thumbnails/thumbnails.service.ts
@@ -4,30 +4,30 @@ import AWS = require('aws-sdk');
 @Injectable()
 export class ThumbnailsService {
   async getThumbnailUrl(userId: string) {
-    const object_name = `thumb/${userId}_240p264kbs.png`;
+    const objectName = `thumb/${userId}_240p264kbs.png`;
     const endpoint = new AWS.Endpoint(process.env.AWS_S3_URL);
     const region = process.env.AWS_S3_REGION;
-    const access_key = process.env.AWS_ACCESS_KEY_ID;
-    const secret_key = process.env.AWS_SECRET_ACCESS_KEY;
+    const accessKey = process.env.AWS_ACCESS_KEY_ID;
+    const secretKey = process.env.AWS_SECRET_ACCESS_KEY;
     const S3 = new AWS.S3({
       endpoint: endpoint,
       region: region,
       credentials: {
-        accessKeyId: access_key,
-        secretAccessKey: secret_key,
+        accessKeyId: accessKey,
+        secretAccessKey: secretKey,
       },
     });
 
     const params = {
       Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: object_name,
+      Key: objectName,
     };
 
     const objectInfo = await S3.getObject(params).promise();
 
     return {
       ContentLength: objectInfo.ContentLength,
-      thumbnailUrl: object_name,
+      thumbnailUrl: objectName,
     };
   }
 }
