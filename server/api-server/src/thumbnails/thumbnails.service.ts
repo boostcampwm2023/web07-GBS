c2b6b5fce049e11bd7e33730fa22888e3a67eb07
Joung MyeongHee <jmhee3411@gmail.com>
2023-12-07 21:39:30 +0900

    fix: thumbnail API 예외처리

--- server/api-server/src/thumbnails/thumbnails.service.ts
+++ server/api-server/src/thumbnails/thumbnails.service.ts
@@ -1,33 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import AWS = require('aws-sdk');
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class ThumbnailsService {
+
+  private readonly logger = new Logger(ThumbnailsService.name);
+
   async getThumbnailUrl(userId: string) {
-    const objectName = `${process.env.AWS_S3_URL}${process.env.AWS_S3_BUCKET_NAME}/thumb/${userId}_240p264kbs.png`;
-    const endpoint = new AWS.Endpoint(process.env.AWS_S3_URL);
-    const region = process.env.AWS_S3_REGION;
-    const accessKey = process.env.AWS_ACCESS_KEY_ID;
-    const secretKey = process.env.AWS_SECRET_ACCESS_KEY;
-    const S3 = new AWS.S3({
-      endpoint: endpoint,
-      region: region,
-      credentials: {
-        accessKeyId: accessKey,
-        secretAccessKey: secretKey,
-      },
-    });
+    try{
+      const S3 = new AWS.S3({
+        endpoint: new AWS.Endpoint(process.env.AWS_S3_URL),
+        region: process.env.AWS_S3_REGION,
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        },
+      });
+      
+      const objectName = `thumb/${userId}_240p264kbs.png`;
+      const thumbnailUrl = `${process.env.AWS_S3_URL}${process.env.AWS_S3_BUCKET_NAME}/thumb/${userId}_240p264kbs.png`;
+      const params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: objectName,
+      };
 
-    const params = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: objectName,
-    };
+      const objectInfo = await S3.getObject(params).promise();
 
-    const objectInfo = await S3.getObject(params).promise();
-
-    return {
-      contentLength: objectInfo.ContentLength,
-      url: objectName,
-    };
+      return {      
+        contentLength: objectInfo.ContentLength,
+        url: thumbnailUrl,
+      };
+    } catch(err) {
+      this.logger.error(err);
+      throw new HttpException('Thumbnail not found', 500)
+    }
   }
 }
