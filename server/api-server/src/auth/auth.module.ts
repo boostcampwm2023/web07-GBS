d5359a4575d69bc62dc95a545b4685bda9744163
Kim KyeongGeun <kkg05102@gmail.com>
2023-12-07 22:07:48 +0900

    [fix] AuthModule에 누락된 GoogleStrategy 추가 (#176)
    fix: AuthModule에 GoogleStrategy 추가

--- server/api-server/src/auth/auth.module.ts
+++ server/api-server/src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { NaverStrategy } from './strategy/naver.strategy';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { GoogleStrategy } from './strategy/google.strategy';
 
 @Module({
   imports: [
@@ -19,7 +20,7 @@ import { JwtModule } from '@nestjs/jwt';
     }),
   ],
   controllers: [AuthController],
-  providers: [NaverStrategy, AuthService],
+  providers: [NaverStrategy, GoogleStrategy, AuthService],
   exports: [PassportModule, JwtModule, AuthService],
 })
 export class AuthModule {}
