c11850a3d1382c7ae144d4f1ac498f3bd0408c09
Joung MyeongHee <jmhee3411@gmail.com>
2023-12-15 11:43:17 +0900

    [fix] 로그인 jwt 코드 제거 (#231)
    * fix: 로그인 jwt 코드 제거
    
    * fix: remove jwt dependecies

--- server/api-server/src/auth/auth.module.ts
+++ server/api-server/src/auth/auth.module.ts
@@ -5,22 +5,17 @@ import { UsersModule } from 'src/users/users.module';
 import { NaverStrategy } from './strategy/naver.strategy';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
 import { GoogleStrategy } from './strategy/google.strategy';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule.register({
-      defaultStrategy: 'jwt',
       session: true,
-    }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
     }),
   ],
   controllers: [AuthController],
   providers: [NaverStrategy, GoogleStrategy, AuthService],
-  exports: [PassportModule, JwtModule, AuthService],
+  exports: [PassportModule, AuthService],
 })
 export class AuthModule {}
